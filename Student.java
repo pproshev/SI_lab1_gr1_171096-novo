3da2d0bb4d3f30b59d127b1a106b33eea25e8fa2
Petar <petar.proshev@yahoo.com>
2020-04-12 15:21:06 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -4,14 +4,20 @@ class Student {
 	String lastName;
 	List<Integer> labPoints;
 	//TODO constructor
-
+	Student(String index,String firstName,String lastName){
+		index=this.index;
+		firstName=this.firstName;
+		lastName=this.lastName;
+		}
 	//TODO seters & getters
-
+	Geteri i Seteri...
 	public double getAverage() {
 		//TODO
+		return (float)labPoints/nbrOfLabs; 
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		return if(labPts>8);
 	}
 }
